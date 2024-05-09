024a95f7ead88f04f20337a4d0443247ff020793
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2024-05-09 12:11:05 +0530

    bundlers

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 - 1) created element using React.createElement()
 - 2) Created root using ReactDOM.createRoot(document.getElementbyId());
 - 3) Rendered the element using root.render(ele)
+
+# using Bundler (Webpack , Vite , Parcel )
+- 1) This app cannot go to production because it is not optimised 
+- 2) There are lot of things other than react to make our app fast 
+- 3) first install npm install -D parcel 
+- 4) D is used here for the Development dependency 
+- 5) npm is for installing package/npx is for exectuing package 
+- 6) 
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,14 @@
+//let h1Tag = React.createElement("h1",{id : "h1tag", className : "classtag"}, "Hello world from React");
+let h1tag = React.createElement("div", {
+    className: "parent"
+}, React.createElement("div", {
+    className: "child"
+}, React.createElement("div", {
+    className: "box"
+}, "I am a box ")));
+// in this part I am creating the nested component  
+console.log(h1tag); // this H1tag is React element ( object )
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1tag); // render work is to create the object( react element ) into HTML and put it into the HTML
+
+//# sourceMappingURL=index.6bd02f5a.js.map
